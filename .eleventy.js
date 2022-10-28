ff676b933e8e4ad29d84b82dc53c98ac943f770f
Henry Desroches <yo@henry.codes>
2022-10-27 22:12:31 -0600

    🔠 - Add font files

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@
 module.exports = (eleventyConfig) => {
   eleventyConfig.htmlTemplateEngine = 'njk';
   eleventyConfig.addPassthroughCopy('src/css');
+  eleventyConfig.addPassthroughCopy('src/fonts');
   eleventyConfig.addPassthroughCopy('src/assets');
 
   return {
